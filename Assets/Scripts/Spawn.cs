2a16855020566f87a655e2936123b722a8db784b
Ildar1982 <68797569+Ildar1982@users.noreply.github.com>
2020-12-21 13:37:39 +0500

    FlyInit

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private int _numberSecondsBetweenOutliersondsfor;
+
+    private float _time = 0;
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time >= _numberSecondsBetweenOutliersondsfor)
+        {
+            Instantiate(_projectile, _spawnPoint);
+            _time = 0;
+        }
+    }
+}
